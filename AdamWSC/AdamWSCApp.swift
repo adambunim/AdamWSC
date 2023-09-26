897f9867bdc278fe5b4f084d183532ac67cc3168
adam <adam>
2023-09-26 20:55:08 +0300

    Initial Commit

---
+++ AdamWSC/AdamWSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdamWSCApp.swift
+//  AdamWSC
+//
+//  Created by Adam Bunim on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdamWSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
