f5df1f3e40966f13ea6cf92b4bb7419fffa5a883
adam <adam>
2023-09-27 10:05:42 +0300

    example json

--- AdamWSC/views/MatchCell.swift
+++ AdamWSC/views/MatchCell.swift
@@ -11,5 +11,16 @@ struct MatchCell: View {
 }
 
 #Preview {
-    MatchCell(match: Match(WSCGameId: "Lazio@AC Milan-$2023-01-24"))
+    guard let url = Bundle.main.url(forResource: "example", withExtension: "json") else {
+        return Text("no file")
+    }
+    do {
+        let data = try Data(contentsOf: url)
+        let decoder = JSONDecoder()
+        let match = try decoder.decode(Match.self, from: data)
+        return MatchCell(match: match)
+    }
+    catch {
+        return Text(error.localizedDescription)
+    }
 }
