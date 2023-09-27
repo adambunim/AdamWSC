dd6549ad9e4fb96e4b7a8a096a0003b04986682f
adam <adam>
2023-09-27 09:08:01 +0300

    screen title

--- AdamWSC/texts/StringKey.swift
+++ AdamWSC/texts/StringKey.swift
@@ -3,4 +3,5 @@ import Foundation
 public enum StringKey: String {
 
 	case failed_to_load_matches // failed to load matches
+	case games // Games
 }
--- AdamWSC/views/ContentView.swift
+++ AdamWSC/views/ContentView.swift
@@ -7,17 +7,29 @@ struct ContentView: View {
     @State var result: Result<[Match],WSCError>? = nil
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
+            HStack {
+                Spacer()
+                Text(.games)
+                    .font(.headline)
+                Spacer()
+            }
+            .padding()
+            .background(.gray.opacity(0.2))
+            
             switch result {
             case .success(let matches):
-                List(matches, id: \.WSCGameId) {
-                    MatchCell(match: $0)
+                ScrollView {
+                    ForEach(matches, id: \.WSCGameId) {
+                        MatchCell(match: $0)
+                    }
                 }
             case .failure:
                 Text(.failed_to_load_matches)
             case .none:
                 ProgressView()
             }
+            Spacer()
         }
         .onAppear {
             MatchesLoader.load() { res in
