738cefe1c4709aa108e29b87b9a89f7b3546691e
adam <adam>
2023-09-27 13:13:54 +0300

    vertical tabs

--- AdamWSC/views/MatchView.swift
+++ AdamWSC/views/MatchView.swift
@@ -19,10 +19,17 @@ struct MatchView: View {
             .background(.gray.opacity(0.2))
             
             if let pages = match.wscGame?.primeStory?.pages {
-                TabView {
-                    ForEach(pages) {
-                        PageView(page: $0)
+                GeometryReader { proxy in
+                    TabView {
+                        ForEach(pages) {
+                            PageView(page: $0)
+                                .frame(width: proxy.size.width, height: proxy.size.height)
+                                .rotationEffect(.degrees(-90))
+                        }
                     }
+                    .frame(width: proxy.size.height, height: proxy.size.width)
+                    .rotationEffect(.degrees(90), anchor: .topLeading)
+                    .offset(x: proxy.size.width)
                 }
                 .tabViewStyle(.page)
                 .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
