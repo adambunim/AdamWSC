87cd88ef83f1b1a8d1387e1bdfb67e52f3895d9e
adam <adam>
2023-09-27 17:21:59 +0300

    first player from cell

--- AdamWSC/views/MatchCellButton.swift
+++ AdamWSC/views/MatchCellButton.swift
@@ -1,15 +1,39 @@
 
 import SwiftUI
+import AVKit
 
 struct MatchCellButton: View {
     
     let match: Match
-    let onPress: () -> Void
+    @State var player: AVPlayer? = nil
+    let onPress: (AVPlayer?) -> Void
     
     var body: some View {
-        Button(action: onPress) {
+        Button(action: {
+            onPress(player)
+        }) {
             MatchCell(match: match)
         }
         .buttonStyle(.plain)
+        .onAppear {
+            loadPlayer()
+        }
+        .onDisappear {
+            player = nil
+        }
     }
+    
+    func loadPlayer() {
+        guard let page = match.wscGame?.primeStory?.pages.first else {
+            return
+        }
+        guard let videoUrl = page.videoUrl else {
+            return
+        }
+        guard let url = URL(string: videoUrl) else {
+            return
+        }
+        player = AVPlayer(url:  url)
+    }
+    
 }
--- AdamWSC/views/MatchList.swift
+++ AdamWSC/views/MatchList.swift
@@ -5,6 +5,7 @@ import AVKit
 struct MatchList: View {
     
     let matches: [Match]
+    @State var firstPlayer: AVPlayer? = nil
     @State var selectedMatch: Match? = nil
     @State var players: [String:AVPlayer] = [:]
     
@@ -13,12 +14,13 @@ struct MatchList: View {
             LazyVStack {
                 ForEach(0..<matches.count, id:\.self) { i in
                     let iter = matches[i]
-                    MatchCellButton(match: iter) {
+                    MatchCellButton(match: iter) { player in
+                        firstPlayer = player
                         selectedMatch = iter
                     }
                 }
                 .sheet(item: $selectedMatch) { match in
-                    MatchView(match: match, firstPlayer: nil)
+                    MatchView(match: match, firstPlayer: firstPlayer)
                 }
             }
         }
