dee7adeab05ce6c49ea432438fc91c1d803b5ef0
adam <adam>
2023-09-28 10:30:55 +0300

    test loding

--- AdamWSC/data/MatchesLoader.swift
+++ AdamWSC/data/MatchesLoader.swift
@@ -4,27 +4,34 @@ import Foundation
 class MatchesLoader {
     
     static func load(_ completion: @escaping (Result<[Match],WSCError>) -> Void) {
-        guard let url = Bundle.main.url(forResource: "matches", withExtension: "json") else {
-            completion(.failure(.load))
-            return
-        }
         DispatchQueue.global(qos: .userInitiated).async { // simulate server call
-            do {
-                let data = try Data(contentsOf: url)
-                let decoder = JSONDecoder()
-                let matches = try decoder.decode(Matches.self, from: data)
-                let filter = matches.response.filter {
-                    $0.wscGame != nil
-                }
-                DispatchQueue.main.async {
-                    completion(.success(filter))
-                }
-            }
-            catch {
+            guard let matches = loadSync() else {
                 DispatchQueue.main.async {
                     completion(.failure(.load))
                 }
+                return
             }
+            DispatchQueue.main.async {
+                completion(.success(matches))
+            }
+        }
+    }
+    
+    static func loadSync() -> [Match]? {
+        guard let url = Bundle.main.url(forResource: "matches", withExtension: "json") else {
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let matches = try decoder.decode(Matches.self, from: data)
+            let filter = matches.response.filter {
+                $0.wscGame != nil
+            }
+            return filter
+        }
+        catch {
+            return nil
         }
     }
     
