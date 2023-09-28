b9d6b246fd258f72f58f97829068833f0f71c3c1
adam <adam>
2023-09-28 10:43:32 +0300

    text json parse

--- AdamWSCTests/AdamWSCTests.swift
+++ AdamWSCTests/AdamWSCTests.swift
@@ -5,9 +5,21 @@ import XCTest
 final class AdamWSCTests: XCTestCase {
 
     func testJsonParse() throws {
-        
+        let matches = MatchesLoader.loadSync()
+        XCTAssertEqual(matches?.count, 48)
     }
 
+    func testSingleJsonParse() throws {
+        let match = ExampleLoader.load()
+        XCTAssertEqual(match?.teams?.home?.name, "Fulham")
+        XCTAssertEqual(match?.teams?.away?.name, "Tottenham")
+        let pages = match?.wscGame?.primeStory?.pages
+        XCTAssertEqual(pages?.count, 11)
+        let lastPage = pages?.last
+        XCTAssertEqual(lastPage?.homeScore, 0)
+        XCTAssertEqual(lastPage?.awayScore, 1)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
